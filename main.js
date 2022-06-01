0b5f0ef5cfaa39a79fa7b11385225c3b9e46469b
Sergey Skorohod <serik.plus.plus@gmail.com>
2022-06-01 13:56:57 +0300

    First Changes

--- main.js
+++ main.js
@@ -13,17 +13,62 @@
 }
 
 var app = new Vue({
-  el: '#accordionFlushExample',
+  el: '#table',
   data: {
-
+		allOrders:[],
+		filterOrders:[],
+		currentPageEl:[],
+		sort:{},
+		pages:3,
+		currentPage:1,
+		elForPage:2,
+		ifFullElements:false
   },
-	created() {	
-
-	},
 	methods:{
+		getCurrentEl(index){
+			if(index){
+				this.currentPage = index+1
+			}
+			let start = this.currentPage === 1 ? 0 : this.currentPage*this.elForPage-this.elForPage
+			getData(`http://localhost:3000/orders?_start=${start}&_limit=${this.elForPage}`).then(data=>{
+				this.currentPageEl = data	
+			})
+		},
+		getAllEl(){
+			if(this.ifFullElements === false)
+			{
+				this.ifFullElements = true
+				getData(`http://localhost:3000/orders`).then(data=>{
+					data.forEach((element,index) => {
+							this.$set(this.allOrders,index,element)
+					});
+				})
+			}
+		},
+		prevPage(){
+			if(this.currentPage>1)this.currentPage--
+			this.getCurrentEl()
+		},
+		nextPage(){
+			if(this.currentPage<this.pages)this.currentPage++
+			this.getCurrentEl()
+		},
+	},
+ 
 
+	mounted() {	
+		let start = this.currentPage === 1 ? 0 : this.currentPage*this.elForPage-this.currentPage
+			getData(`http://localhost:3000/orders?_start=${start}&_limit=${this.elForPage}`).then(data=>{
+				this.currentPageEl = data	
+			})
 	},
 	computed:{
+		disablePrevPagination(){
+			return 	this.currentPage>1?'':'disabled'
+		},
+		disableNextPagination(){
+			return 	this.currentPage<this.pages ? '':'disabled'
+		}
+	},
 
-	}
 })
